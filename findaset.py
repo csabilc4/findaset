ace1a120b22f42f34c7a6be5c2b9fe35b0e4a945
csabi <nemeth.csaba@revolve.hu>
2018-10-10 07:58:17 +0200

    Initial commit

---
+++ findaset.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+class Cards(object):
+    def __init__(self, colour, fill, form):
+        self.colour = colour
+        self.fill = fill
+        self.form = form
+
+
+    
