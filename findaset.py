9feb3a05fd26958bba5aa740e0d10dc674c748a4
csabi <nemeth.csaba@revolve.hu>
2018-10-11 00:25:07 +0200

    fel vele

--- findaset.py
+++ findaset.py
@@ -2,80 +2,186 @@
 # coding=utf-8
 
 import random
+import itertools
 
-COLOUR = [
-    'Red',
-    'Green',
-    'Purple'
+# shapes = ['1',
+# 		  '2',
+# 		  '3',
+# 		  '4',
+# 		  '5',
+# 		  '6',
+# 		  '7',
+# 		  '8',
+# 		  '9',
+# 		  '10',
+# 		  '11',
+# 		  '12',
+# 		  ]
+# result = itertools.combinations(shapes, 3)
+# for each in result:
+#     print(each)
+#
+# exit()
+
+COLOURS = [
+	'Red',
+	'Green',
+	'Purple'
 ]
 
-FORM = [
-    'Elips',
-    'Amorf',
-    'Tiles'
+FORMS = [
+	'Elips',
+	'Amorf',
+	'Tiles'
 ]
 
-FILL = [
-    'Full',
-    'Strip',
-    'Empty'
+FILLS = [
+	'Full',
+	'Strip',
+	'Empty'
 ]
 
-NUMBER = [
-    'One',
-    'Two',
-    'Three'
+NUMBERS = [
+	'One',
+	'Two',
+	'Three'
 ]
+
+
+
 
 class Cards(object):
-    def __init__(self, colour, fill, form, number):
-        self.colour = colour
-        self.fill = fill
-        self.form = form
-        self.number = number
+	def __init__(self, colour, fill, form, number):
+		self.colour = colour
+		self.fill = fill
+		self.form = form
+		self.number = number
 
-    def randomChoice(self):
-        random.choice(COLOURS)
+	def randomChoice(self):
+		random.choice(COLOURS)
 
 
-def randomCards():
 
-    cards = {}
+def setPackOfCards():
+	packOfCards = {}
+	num = 1
 
-    for c in range(1, 13):
-        randomCardPropertys = [random.choice(COLOUR),\
-        random.choice(FORM),\
-        random.choice(FILL),\
-        random.choice(NUMBER)]
+	for colour in COLOURS:
+		for form in FORMS:
+			for fill in FILLS:
+				for number in NUMBERS:
+					packOfCards[num] = [colour, form, fill, number]
+					num += 1
 
-        cards[c] = randomCardPropertys
-        # print c, cards[c]
+	return packOfCards
+	# print packOfCards
 
-    return cards
-    # randomCard = ''.join(randomCardPropertys)
+# setPackOfCards()
+# exit()
 
+def getRandomCards():
+	packOfCards = setPackOfCards()
+	# print packOfCards
+
+	random_cards = {}
+	randomNumbers = random.sample(range(1, 82), 12)
+	# print randomNumbers
+
+	i = 1
+	for c in randomNumbers:
+		random_cards[i] = packOfCards[c]
+		i += 1
+
+	# for c in range(1, 13):
+	# 	randomCardPropertys = [random.choice(COLOURS), \
+	# 						   random.choice(FORMS), \
+	# 						   random.choice(FILLS), \
+	# 						   random.choice(NUMBERS)]
+	#
+	# 	random_cards[c] = randomCardPropertys
+	# print c, cards[c]
+
+
+	return random_cards
+	# print random_cards
+
+# getRandomCards()
+# exit()
 
 def checkCards(cardsInTable):
-    # print cardsInTable.keys()
-    # print cardsInTable.values()
+	# print cardsInTable.keys()
+	# print cardsInTable.values()
 
-    for cardNum, values in cardsInTable.items():
-        print cardNum, values[0], type(values[0])
-    pass
+	for cardNum, values in cardsInTable.items():
+		print cardNum, values
+		pass
+
+	result = itertools.combinations(cardsInTable.keys(), 3)
+	for each in result:
+		# print each[0], type(each)
+		# sum = len(set(cardsInTable[each[0]] + cardsInTable[each[1]] + cardsInTable[each[2]]))
+		# print each, cardsInTable[each[0]], cardsInTable[each[1]], cardsInTable[each[2]]
+		# print search_intersections(cardsInTable[each[0]], cardsInTable[each[1]])
+
+		search_intersections_of_3(each, cardsInTable[each[0]], cardsInTable[each[1]], cardsInTable[each[2]])
+		if sum == 6 or sum == 8 or sum == 12:
+			# print sum, 'SET!!!'
+			pass
+
+def search_intersections_of_2(set1, set2):
+
+	return set(set1).intersection(set2)
+
+def search_intersections_of_3(combo, set1, set2, set3):
+
+	sum_12 = search_intersections_of_2(set1, set2)
+	sum_13 = search_intersections_of_2(set1, set3)
+	sum_23 = search_intersections_of_2(set2, set3)
+
+	# print sum_12, sum_13, sum_23
+
+	bFoundSet = False
+	if sum_12 == sum_13 and sum_12 == sum_23 and sum_12 != 1:
+		bFoundSet = True
+
+	if bFoundSet == True:
+		print 'SET!!!', combo, sum_12, sum_13, sum_23
 
 
-checkCards(randomCards())
+checkCards(getRandomCards())
 
 
+exit()
+
+
+
+
+
+
+
+list1 = ['Green', 'Elips', 'Empty', 'Three']
+list2 = ['Blue', 'Tiles', 'Strip', 'One']
+list3 = ['Yellow', 'Amorf', 'Full', 'Two']
+
+sum = set(list1 + list2 + list3)
+# print len(sum)
+
+list4 = ['Green', 'Elips', 'Full', 'Three']
+list5 = ['Green', 'Elips', 'Full', 'One']
+list6 = ['Green', 'Elips', 'Full', 'Two']
+
+# sum2 = set(list4 + list5 + list6)
+sum2 =set(list4).intersection(list5)
+print sum2
+# print len(sum2), sum2
 
 # 12db random kártyát kell generálni a pakliból, egyedinek kell lenniük
+# fel kell írni az összes 3-as kombinációt a 12 lapból pl. 123, 124, 125, 129, 267
 # meg kell vizsgálnia a tulajdonságaikat:
 # set:
-# -ha 3 tulajd. megegyezik
-# -ha pontosan 2 tulajd. megegyezik
-# -ha nincs egyező tulajd.
-
-
+# -ha 3 tulajd. megegyezik (ha set-be rakom őket és a len(set) = 6)
+# -ha pontosan 2 tulajd. megegyezik (ha set-be rakom őket és a len(set) = 6)
+# -ha nincs egyező tulajd. (ha set-be rakom őket és a len(set) = 12)
 
 
 redFullElipsOne = Cards('red', 'full', 'elips', 'one')
@@ -115,11 +221,9 @@ greenEmptyTilesOne = Cards('green', 'empty', 'Tiles', 'one')
 purpleEmptyTilesOne = Cards('purple', 'empty', 'Tiles', 'one')
 
 
-
-
 def mix():
-    '''random kiválaszt kártáykat'''
+	'''random kiválaszt kártáykat'''
 
-    return random.choice(0, 1)
+	return random.choice(0, 1)
 
-    pass
+	pass
